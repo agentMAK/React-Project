fb5f1b918cc1505a887f84083a4d81808cc7b5f6
martin0044 <martin0044@gmail.com>
2021-11-02 10:08:47 +0000

    Added first component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <h2>This is Martin</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseItem() {
+    return <h2>This is the expense componment</h2>
+}
+
+export default ExpenseItem;
